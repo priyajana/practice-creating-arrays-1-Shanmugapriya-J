7195cf0eeb7ec4404bd1c69640789b1b1a751560
Shanmugapriya <priyajanarthanan@gmail.com>
2025-03-04 14:27:02 -0600

    File added for Practice : Creating Arrays

---
+++ PracticeArray.js
@@ -0,0 +1,15 @@
+let groceryItems=["Rice","Cereal","Butter","Milk","Bread"];
+let prices = [101.89,29345.66,3234.54];
+
+//printing the arrays
+
+console.log(groceryItems);
+console.log(prices);
+let i=0;
+while(i<groceryItems.length)
+    {
+        console.log(groceryItems[i]);
+        i+=2;
+    }
+console.log(prices[0]);
+console.log(prices[2]);
